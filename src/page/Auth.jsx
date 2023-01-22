5ccdec984c4b64999735b14342718c7adee71ebf
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-01-23 00:50:05 +0900

    fix: auth 로그인 회원가입 기능

--- src/page/Auth.jsx
+++ src/page/Auth.jsx
@@ -1,48 +1,72 @@
-import { Button, TextField } from '@mui/material'
-import React, { useState, useRef } from 'react'
-import styled from 'styled-components'
-
-const Contaier = styled.div`
-   display: flex;
-   flex-direction: column;
-    align-items: center;
-    height:100vh;
-    justify-content: center;
-
-`
+import { Button, TextField, Typography } from '@mui/material';
+import React, { useState, useRef } from 'react';
+import { authService } from '../firebase';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
 
 function Auth() {
-    const [auth, setAuth] = useState("회원가입")
-    const idRef = useRef("")
-    const passwordRef = useRef("")
-    const onClickBtn = () => {
-        console.log(idRef.current, passwordRef)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [auth, setAuth] = useState('로그인');
+  const [error, setError] = useState('');
+  const onChange = event => {
+    const {
+      target: { type, value },
+    } = event;
+    if (type === 'email') {
+      setEmail(value);
+    } else if (type === 'password') {
+      setPassword(value);
     }
-    const onClickAuth = () => {
-        setAuth((prev) => prev === "로그인" ? "회원가입" : "로그인")
+  };
+  const handleAuthEvent = async event => {
+    event.preventDefault();
+    try {
+      if (auth === '로그인') {
+        await signInWithEmailAndPassword(authService, email, password);
+      } else if (auth === '회원가입') {
+        await createUserWithEmailAndPassword(authService, email, password);
+      }
+    } catch (error) {
+      setError(error.message);
     }
-    return (
-        <Contaier>
-            <TextField
-                ref={idRef}
-                id="standard-required"
-                label="아이디"
-                variant="standard"
+  };
+  const toggleAuth = () => {
+    setAuth(prev => (prev === '로그인' ? '회원가입' : '로그인'));
+    setError('');
+  };
 
+  return (
+    <>
+      <TextField
+        id="standard-required"
+        type="email"
+        name="email"
+        label="이메일"
+        variant="standard"
+        onChange={onChange}
+      />
+      <TextField
+        id="standard-password-input"
+        label="비밀번호"
+        type="password"
+        name="password"
+        autoComplete="current-password"
+        variant="standard"
+        onChange={onChange}
+      />
+      {error && <Typography>이메일과 비밀번호를 다시 확인해주세요</Typography>}
+      <Button variant="contained" onClick={handleAuthEvent}>
+        {auth === '로그인' ? '로그인' : '회원가입'}
+      </Button>
 
-            />
-            <TextField
-                ref={passwordRef}
-                id="standard-password-input"
-                label="비밀번호"
-                type="password"
-                autoComplete="current-password"
-                variant="standard"
-            />
-            <Button variant="text" onClick={onClickBtn}>{auth === "로그인" ? "회원가입" : "로그인"}</Button>
-            <Button variant="text" color='secondary' onClick={onClickAuth}>{auth}</Button>
-        </Contaier>
-    )
+      <Button variant="text" color="secondary" onClick={toggleAuth}>
+        {auth === '로그인' ? '회원가입' : '로그인'}
+      </Button>
+    </>
+  );
 }
 
-export default Auth
+export default Auth;
