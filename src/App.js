e2a03fa1ca033d76c68e3b65950fe239f2d2186e
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-01-23 00:48:04 +0900

    fix: login 상태에 따라서 로그인 상태값 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import { Routes, Route } from 'react-router-dom';
-import Auth from './page/Auth';
+import { authService } from './firebase';
+import AppRouter from './components/Router/Router';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  justify-content: center;
+`;
+
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged(user => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Auth />} />
-    </Routes>
+    <Container>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'loding'}
+    </Container>
   );
 }
 
