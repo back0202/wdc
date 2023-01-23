11fe411aa1d98d3af56403a35e84b18fc6877f04
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-01-23 18:41:24 +0900

    add: firebase setting

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +24,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const authService = getAuth(app);
+export const dbService = getFirestore(app);
