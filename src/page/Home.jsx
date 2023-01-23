ad6de3eab1e8b103f721ec099b8eb0e5ea436606
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-01-23 18:40:39 +0900

    fix: home컴포넌트 firebase snapshot 추가

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,18 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ItemList from 'components/ItemList';
+import List from '@mui/material/List';
+import MemberAdd from '../components/MemberAdd';
+import MemberUpdate from '../components/MemberUpdate';
+import MemberDelete from '../components/MemberDelete';
+import { memberApi } from 'apis/member/member-api';
+import { collection, onSnapshot, getFirestore } from 'firebase/firestore';
 
+const dbService = getFirestore();
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -40,27 +47,82 @@ function a11yProps(index) {
   };
 }
 
-export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
+export default function Home({ userObject }) {
+  const [value, setValue] = useState(0);
+  const [member, setMember] = useState([]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const getAllMember = async () => {
+    const allMember = await memberApi.getMember();
+    setMember([...allMember]);
+  };
+
+  useEffect(() => {
+    console.log(member);
+  }, [member]);
+
+  useEffect(() => {
+    onSnapshot(collection(dbService, 'Member'), snapShot => {
+      const memberList = snapShot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMember(memberList);
+    });
+  }, []);
+
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="fullWidth"
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          variant="fullWidth"
         >
           <Tab label="회원목록" {...a11yProps(0)} />
           <Tab label="3주간 미참석" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <ItemList />
+        <MemberAdd />
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          {member &&
+            member.map(element => {
+              console.log(element);
+              return (
+                <ItemList
+                  id={element.id}
+                  name={element.name}
+                  birthday={element.birth}
+                >
+                  <Box>
+                    <MemberUpdate
+                      id={element.id}
+                      nameValue={element.name}
+                      birthValue={element.birth}
+                    />
+                    <MemberDelete
+                      id={element.id}
+                      nameValue={element.name}
+                      birthValue={element.birth}
+                    />
+                  </Box>
+                </ItemList>
+              );
+            })}
+        </List>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <ItemList />
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          <ItemList name="뉴진스" birthday="2022.01.02">
+            <button>수정</button>
+            <button>삭제</button>
+          </ItemList>
+        </List>
       </TabPanel>
     </Box>
   );
