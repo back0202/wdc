a3bfcfe021cead67ecb3b506b87d31c3f0f04248
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-01-23 18:40:17 +0900

    fix: router에 userObject 추가

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from '../../page/Auth';
 import Home from '../../page/Home';
-function AppRouter({ isLoggedIn }) {
+function AppRouter({ isLoggedIn, userObject }) {
   return (
     <Routes>
       {isLoggedIn ? (
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home userObject={userObject} />} />
       ) : (
         <Route path="/" element={<Auth />} />
       )}
